389c3f50eabd2c689c35b76d6447e23a3df2476e
Gleb Zykov <g.zykov@gtc-spb.ru>
2017-10-31 14:36:06 +0300

    remove comments from truffle.js

--- truffle.js
+++ truffle.js
@@ -7,19 +7,16 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      //from: '0xfe00a80d73045eb7c4edbdd4fe2f3c9f11ef9d5e'
     },
     ropsten: {
      network_id: 3,
      host: "localhost",
      port:  8545,
-     // gasprice: 500000000
     },
     rinkeby: {
      network_id: 4,
      host: "localhost",
      port:  8545,
-     // gasprice: 500000000
     },
 
   }
